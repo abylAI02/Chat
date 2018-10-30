beeab62ca1109f326be96d16d8ac947edf625a4c
HungryPants <yelamanvalikhanovich@gmail.com>
2018-10-31 02:46:40 +0600

    Chat with Sign in and Sign up operations.

---
+++ app/src/main/java/com/example/yelaman/chat/LogInActivity.java
@@ -0,0 +1,99 @@
+package com.example.yelaman.chat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button signIn;
+    private TextView signUp;
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "TAG" + LogInActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        email = findViewById(R.id.editTextMail);
+        password = findViewById(R.id.editTextPassword);
+        signIn = findViewById(R.id.buttonSignIn);
+        signUp = findViewById(R.id.textViewSignUp);
+        mAuth = FirebaseAuth.getInstance();
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View v) {
+                signUp.setTextColor(R.color.colorBlack);
+                changeActivity();
+            }
+        });
+    }
+
+    private void changeActivity() {
+        Intent intent = new Intent(LogInActivity.this , SignUpActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void login()  {
+        String email = String.valueOf(this.email.getText());
+        String password = String.valueOf(this.password.getText());
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(
+                                    LogInActivity.this,
+                                    "Authentication failed.",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+        /*Intent intent = new Intent(this, .class);
+        // Toast.makeText(this, user.getEmail(), Toast.LENGTH_SHORT).show();
+        startActivity(intent);*/
+
+    }
+}
---
+++ app/src/main/java/com/example/yelaman/chat/SignUpActivity.java
@@ -0,0 +1,144 @@
+package com.example.yelaman.chat;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText name, email, password, age;
+    private Button signUp;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
+
+    private static final String TAG = "TAG" + SignUpActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        name = findViewById(R.id.editTextName);
+        email = findViewById(R.id.editTextRegMail);
+        password = findViewById(R.id.editTextRegPassword);
+        age = findViewById(R.id.editTextAge);
+        signUp = findViewById(R.id.buttonRegister);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
+    }
+
+    private void register() {
+        final String email = String.valueOf(this.email.getText());
+        final String password = String.valueOf(this.password.getText());
+        final String age = String.valueOf(this.age.getText());
+        final String name = String.valueOf(this.name.getText());
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(
+                                    SignUpActivity.this,
+                                    "Authentication OK.",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(
+                                    SignUpActivity.this,
+                                    "Authentication failed.",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }
+                });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef =
+                database.getReference("users/" + mUser.getUid());
+        myRef.updateChildren(new HashMap<String, Object>() {{
+                put("age", age);
+                put("name", name);
+        }}).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(SignUpActivity.this, task.isSuccessful() + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                Toast.makeText(
+                        SignUpActivity.this,
+                        dataSnapshot.getKey() + ":" +
+                                dataSnapshot.getValue(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
+
+}
