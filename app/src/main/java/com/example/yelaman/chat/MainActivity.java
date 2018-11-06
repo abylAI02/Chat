f030d16ca5c51600d21e0cbde7a3c46c53e01d3b
HungryPants <yelamanvalikhanovich@gmail.com>
2018-11-06 23:33:02 +0600

    Navigation drawer was added.

--- app/src/main/java/com/example/yelaman/chat/MainActivity.java
+++ app/src/main/java/com/example/yelaman/chat/MainActivity.java
@@ -5,11 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +15,26 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_chat:
+                    setTitle("Chat");
+                    ChatFragment chatFragment = new ChatFragment();
+                    android.support.v4.app.FragmentTransaction chatFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    chatFragmentTransaction.replace(R.id.frame,chatFragment , "ChatFragment");
+                    chatFragmentTransaction.commit();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_news:
+                    setTitle("News");
+                    NewsFragment newsFragment = new NewsFragment();
+                    android.support.v4.app.FragmentTransaction newsFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    newsFragmentTransaction.replace(R.id.frame,newsFragment , "ChatFragment");
+                    newsFragmentTransaction.commit();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_user:
+                    setTitle("User");
+                    UserFragment userFragment = new UserFragment();
+                    android.support.v4.app.FragmentTransaction userFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    userFragmentTransaction.replace(R.id.frame,userFragment , "ChatFragment");
+                    userFragmentTransaction.commit();
                     return true;
             }
             return false;
@@ -36,9 +46,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage =  findViewById(R.id.message);
         BottomNavigationView navigation =  findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        setTitle("Chat");
+        ChatFragment chatFragment = new ChatFragment();
+        android.support.v4.app.FragmentTransaction chatFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        chatFragmentTransaction.replace(R.id.frame, chatFragment,"ChatFragment");
+        chatFragmentTransaction.commit();
     }
 
 }
